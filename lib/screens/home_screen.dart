019f9a665972e2e227df4c70461a60601444a36f
VinayPapnoi <papnoi.vinay@gmail.com>
2025-10-01 00:21:20 +0530

    added the logout button in the home page

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:brl_task_2/models/country_model.dart';
 import 'package:brl_task_2/services/api_service.dart';
@@ -72,109 +73,147 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16),
-        child: Center(
-          child: Column(
-            children: [
-              TextField(
-                controller: _controller,
-                style: const TextStyle(fontSize: 25),
-                onChanged: _getSuggestions,
-                decoration: const InputDecoration(
-                  labelText: 'Enter country name',
-                  labelStyle: TextStyle(fontSize: 20),
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              if (_suggestions.isNotEmpty)
-                Container(
-                  //neeche waali line recheck karni hai ek baar
-                  margin: const EdgeInsets.all(5),
-                  padding: const EdgeInsets.all(5),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    border: Border.all(color: Colors.grey.shade400),
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  child: SizedBox(
-                    height: 200,
-                    child: ListView(
-                      shrinkWrap: true,
-                      children: _suggestions.map((country) {
-                        return ListTile(
-                          title: Text(
-                            country.name,
-                            style: const TextStyle(fontSize: 18),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment
+              .spaceBetween, // space between top content and bottom
+          children: [
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    TextField(
+                      controller: _controller,
+                      style: const TextStyle(fontSize: 25),
+                      onChanged: _getSuggestions,
+                      decoration: const InputDecoration(
+                        labelText: 'Enter country name',
+                        labelStyle: TextStyle(fontSize: 20),
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    if (_suggestions.isNotEmpty)
+                      Container(
+                        margin: const EdgeInsets.all(5),
+                        padding: const EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          border: Border.all(color: Colors.grey.shade400),
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: SizedBox(
+                          height: 200,
+                          child: ListView(
+                            shrinkWrap: true,
+                            children: _suggestions.map((country) {
+                              return ListTile(
+                                title: Text(
+                                  country.name,
+                                  style: const TextStyle(fontSize: 18),
+                                ),
+                                onTap: () {
+                                  _controller.text = country.name;
+                                  _searchCountry();
+                                },
+                              );
+                            }).toList(),
                           ),
-                          onTap: () {
-                            _controller.text = country.name;
-                            _searchCountry();
-                          },
-                        );
-                      }).toList(),
+                        ),
+                      ),
+                    const SizedBox(height: 10),
+                    AnimatedContainer(
+                      duration: const Duration(milliseconds: 300),
+                      width: _pressed ? 220 : 180,
+                      height: 55,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            _pressed = true;
+                          });
+                          Future.delayed(const Duration(milliseconds: 300), () {
+                            if (mounted) {
+                              setState(() {
+                                _pressed = false;
+                              });
+                            }
+                          });
+                          _searchCountry();
+                        },
+                        style: ElevatedButton.styleFrom(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 40,
+                            vertical: 10,
+                          ),
+                        ),
+                        child: const Text(
+                          'Search',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-              const SizedBox(height: 10),
-              AnimatedContainer(
-                duration: const Duration(milliseconds: 300),
-                width: _pressed ? 220 : 180,
-                height: 55,
-                child: ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      _pressed = true;
-                    });
-
-                    Future.delayed(const Duration(milliseconds: 300), () {
-                      if (mounted) {
-                        setState(() {
-                          _pressed = false;
-                        });
-                      }
-                    });
-                    _searchCountry();
-                  },
-                  style: ElevatedButton.styleFrom(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 40,
-                      vertical: 10,
-                    ),
-                  ),
-                  child: const Text(style: TextStyle(fontSize: 20), 'Search'),
+                    const SizedBox(height: 20),
+                    if (_isLoading)
+                      Image.asset(
+                        'assets/loading.gif',
+                        width: 300,
+                        height: 300,
+                      ),
+                    if (_error.isNotEmpty)
+                      Text(
+                        _error,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromARGB(255, 255, 17, 0),
+                          fontSize: 40,
+                        ),
+                      ),
+                    if (_country != null) ...[
+                      if (_country!.flagUrl.isNotEmpty)
+                        Image.network(_country!.flagUrl, height: 150),
+                      const SizedBox(height: 10),
+                      Text(
+                        'Name: ${_country!.name}',
+                        style: const TextStyle(fontSize: 22),
+                      ),
+                      Text(
+                        'Capital: ${_country!.capital}',
+                        style: const TextStyle(fontSize: 22),
+                      ),
+                      Text(
+                        'Continent: ${_country!.region}',
+                        style: const TextStyle(fontSize: 22),
+                      ),
+                      Text(
+                        'Population: ${_country!.population}',
+                        style: const TextStyle(fontSize: 22),
+                      ),
+                    ],
+                  ],
                 ),
               ),
-              const SizedBox(height: 20),
-              if (_isLoading)
-                Image.asset('assets/loading.gif', width: 300, height: 300),
-              if (_error.isNotEmpty)
-                Text(
-                  _error,
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Color.fromARGB(255, 255, 17, 0),
-                    fontSize: 40,
-                  ),
+            ),
+
+            ElevatedButton(
+              onPressed: () async {
+                await FirebaseAuth.instance.signOut();
+                Navigator.of(context).pushReplacementNamed('/login');
+              },
+              style: ElevatedButton.styleFrom(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 40,
+                  vertical: 10,
                 ),
-              if (_country != null) ...[
-                if (_country!.flagUrl.isNotEmpty)
-                  Image.network(_country!.flagUrl, height: 150),
-                const SizedBox(height: 10),
-                Text(style: TextStyle(fontSize: 22), 'Name: ${_country!.name}'),
-                Text(
-                  style: TextStyle(fontSize: 22),
-                  'Capital: ${_country!.capital}',
+                backgroundColor: Colors.red,
+              ),
+              child: const Text(
+                'Logout',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
                 ),
-                Text(
-                  style: TextStyle(fontSize: 22),
-                  'Continent: ${_country!.region}',
-                ),
-                Text(
-                  style: TextStyle(fontSize: 22),
-                  'Population: ${_country!.population}',
-                ),
-              ],
-            ],
-          ),
+              ),
+            ),
+          ],
         ),
       ),
     );
